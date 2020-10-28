8ab640d5e3b9acc8a3ffa30c1d026649467ddd23
Amber Lochner <alochner@hawk.iit.edu>
2020-10-27 20:14:17 -0500

    Add line to check what our error was

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -31,8 +31,10 @@ class ProductsController < ApplicationController
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
+        puts @product.errors.full_messages
         format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.json { render json: @product.errors,
+          status: :unprocessable_entity }
       end
     end
   end
