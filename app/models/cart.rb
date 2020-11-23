cc75295099d430c95c9bc8f5691c47f8bdede72d
Amber Lochner <alochner@hawk.iit.edu>
2020-11-23 10:27:16 -0600

    Add closing statement to debug

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,6 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
-end
+
 
 def add_product(product)
   current_item = line_items.find_by(product_id: product.id)
@@ -10,4 +10,5 @@ def add_product(product)
     current_item = line_items.build(product_id: product.id)
   end
   current_item
+ end
 end
