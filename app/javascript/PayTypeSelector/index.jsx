685d861f2b5326e2cd3cde3e49c72df27615645f
Amber Lochner <alochner@hawk.iit.edu>
2020-11-27 15:14:17 -0600

    Debug to fix error

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
 class PayTypeSelector extends React.Component {
-  render() {
+  onPayTypeSelected(event) {
+    console.log(event.target.value);
+ }
     return (
       <div className="field">
       <label htmlFor="order_pay_type">Pay type</label>
-      <select id="order_pay_type" name="order[pay_type]">
-      <option value="">Select a payment method</option>
-      <option value="Check">Check</option>
-      <option value="Credit card">Credit card</option>
-      <option value="Purchase order">Purchase order</option>
+      <select onChange={this.onPayTypeSelected} name="order[pay_type]">
+        <option value="">Select a payment method</option>
+        <option value="Check">Check</option>
+        <option value="Credit card">Credit card</option>
+        <option value="Purchase order">Purchase order</option>
         </select>
       </div>
     );
